aaecda0eb7aaa818cf05481ce9acb4cf36ddfe27
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-12-06 15:38:36 -0600

    update version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ packages = find_packages()
 
 setup(
     name="image-generator",
-    version="1.0.4",
+    version="1.1.0",
     author="Open Baton Dev",
     author_email="dev@openbaton.org",
     description="The Image Generator",
